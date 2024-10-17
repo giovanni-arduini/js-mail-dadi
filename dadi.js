55ae88f73d640c61983073926f137739541238df
giovanni-arduini <gionnibigudd@gmail.com>
2024-10-17 15:44:18 +0200

    ADD userNumber random number generator

--- dadi.js
+++ dadi.js
@@ -3,7 +3,12 @@ sia per il giocatore sia per il computer.
 Stabilire il vincitore, in base a chi 
 fa il punteggio più alto. */
 
-// Generare un numero random per l'utente
+// Generare un numero random da 1 a 6 per l'utente
+// posso crearlo con Math.random
+// limito lo span fino a 0.6, moltiplico per 10 e lo rendo intero
+
+let userNumber = Math.floor(Math.random(0.1, 0.6) * 10); //number
+console.log(userNumber);
 
 // generare un numero random per il CPU
 
