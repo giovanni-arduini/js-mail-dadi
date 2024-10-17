b88be2596482aca033e3aaedfd2755a459d43248
giovanni-arduini <gionnibigudd@gmail.com>
2024-10-17 15:09:13 +0200

    ADD pseudo code for mail.js

---
+++ mail.js
@@ -0,0 +1,15 @@
+/* Chiedi all’utente la sua email, controlla che sia 
+nella lista di chi può accedere, stampa un messaggio 
+appropriato sull’esito del controllo.
+ */
+
+// Lista indirizzi email con accesso
+
+// Chiedere all'utente la sua email
+
+// Confrontare la mail con gli elementi della lista
+
+// SE la mail dell'utente è presente nella lista
+//  => Dare messaggio di avvenuto accesso
+// ALTRIMENTI
+//  => Dare messaggio di accesso negato
