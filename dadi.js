5fcc0b70cf8b1ee72991b8ec12d85c7c511021e8
giovanni-arduini <gionnibigudd@gmail.com>
2024-10-17 15:34:55 +0200

    ADD dadi.js & pseudo code

---
+++ dadi.js
@@ -0,0 +1,16 @@
+/* Generare un numero random da 1 a 6, 
+sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi 
+fa il punteggio più alto. */
+
+// Generare un numero random per l'utente
+
+// generare un numero random per il CPU
+
+// confrontare i due numeri
+
+// SE il numero dell'utente è più alto
+//  => mostrare messaggio di vittoria utente
+
+// ALTRIMENTI
+//  => mostrare messaggio di vittoria CUP
