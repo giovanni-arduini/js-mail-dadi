042e1546fcb0f249a384af268f23cd4574a6a22c
giovanni-arduini <gionnibigudd@gmail.com>
2024-10-18 10:07:22 +0200

    ADD corrections mails

--- mail.js
+++ mail.js
@@ -12,7 +12,7 @@ const emailList = [
 
 // Chiedere all'utente la sua email
 
-let userEmail = prompt("Inserisci il tuo indirizzo e-mail");
+let userEmail = prompt("Inserisci il tuo indirizzo e-mail"); //string
 
 // Verificare che il dato sia una email
 
@@ -27,3 +27,40 @@ if (emailList.includes(userEmail)) {
   //  => Dare messaggio di accesso negato
   console.log(`Accesso negato. L'indirizzo ${userEmail} non è abilitato.`);
 }
+
+// la logica dell'.includes si potrebbe
+// rifare a mano con un ciclo for
+
+//inizializzo la varaibile di identità con false
+// let found = false;
+
+// for (let i = 0; i > emailList.length; i++) {
+//   const currentMail = emailList[i];
+//   console.log(currentMail === userEmail);
+
+//   found = currentMail === userEmail;
+
+// solo quando c'è identità la variabile cambia valore
+// if (currentMail === userEmail) {
+//   found = true;
+//    break
+// }
+// OPPURE
+//
+// // if (currentMail !== userEmail) {
+//   continue
+// }
+// }
+
+// console.log(found);
+
+// // SE la mail è stata trovata
+// // => hai accesso
+// if ((found = true)) {
+//   console.log("Hai accesso alla pagina");
+// }
+// // ALTRIMENTI
+// //  =>  di accesso negato
+// else {
+//   console.log("Accesso negato");
+// }
